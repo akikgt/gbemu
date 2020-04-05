98c1f05819f6c1646a88364107ce27530c47ca3d
akikgt <komoria@oregonstate.edu>
2020-04-05 17:51:46 +0900

    Improve debug mode

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"gbemu/cpu"
 	"gbemu/mmu"
+	"os"
 )
+
+func debugMode(cpu *cpu.CPU, breakPoint *uint16) bool {
+	fmt.Printf("_")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	switch input {
+	case "d":
+		cpu.Dump()
+		return debugMode(cpu, breakPoint)
+	case "n":
+		cpu.Execute()
+		*breakPoint = cpu.GetPC()
+		return true
+	case "c":
+		// loop until BIOS end
+		*breakPoint = 0x100
+		return true
+	case "q":
+		// quit
+		return false
+	default:
+		return false
+	}
+}
 
 func main() {
 	mmu := mmu.New()
 	cpu := cpu.New(mmu)
-	var breakPoint uint16 = 0x89
+	var breakPoint uint16 = 0x93
 
 	for {
 		fmt.Printf("%#04x : ", cpu.GetPC())
 
 		if cpu.GetPC() == breakPoint {
-			cpu.Dump()
-			break
+			isContinue := debugMode(cpu, &breakPoint)
+			if !isContinue {
+				break
+			}
+		} else {
+			cpu.Execute()
 		}
-		cpu.Execute()
-		// cpu.Dump()
+
 	}
 }
