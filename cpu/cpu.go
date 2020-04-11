c73c8a80601d0639157d70586f8766b5b53b9e1e
akikgt <komoria@oregonstate.edu>
2020-04-11 18:35:36 +0900

    Add basic ticks(not yet conditional ticks)

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -18,7 +18,7 @@ func printWord(opcode uint16) {
 
 type CPU struct {
 	mmu   *mmu.MMU
-	clock int
+	ticks uint32
 
 	// registers
 	a byte // accumulator
@@ -58,6 +58,7 @@ func (cpu *CPU) Dump() {
 	fmt.Printf("H: %#02x L: %#02x\n", cpu.h, cpu.l)
 	fmt.Printf("SP: %#04x\n", cpu.sp)
 	fmt.Printf("PC: %#04x\n", cpu.pc)
+	fmt.Printf("Ticks: %08d\n", cpu.ticks)
 	fmt.Println("--------------------")
 }
 
