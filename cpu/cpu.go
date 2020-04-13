88e28ecbdfe52034a780bb020089b3a1dfc27b30
akikgt <komoria@oregonstate.edu>
2020-04-13 23:01:12 +0900

    Add loading ROM functionality

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -60,8 +60,44 @@ func (cpu *CPU) Dump() {
 	fmt.Printf("SP: %#04x\n", cpu.sp)
 	fmt.Printf("PC: %#04x\n", cpu.pc)
 	fmt.Printf("TotalTicks: %08d\n", cpu.TotalTicks)
-	fmt.Printf("scy: %08d\n", cpu.mmu.Read(0xff42))
+	fmt.Printf("lcdc: %02x\n", cpu.mmu.Read(0xff40))
+	fmt.Printf("stat: %02x\n", cpu.mmu.Read(0xff41))
+	fmt.Printf("ly: %02x\n", cpu.mmu.Read(0xff44))
+	fmt.Printf("instruction: %02x\n", cpu.mmu.Read(cpu.pc))
 	fmt.Println("--------------------")
+	// fmt.Println("HRAM")
+	// fmt.Println("--------------------")
+	// fmt.Printf("[0xff05] = %#02x\n", cpu.mmu.Read(0xff05))
+	// fmt.Printf("[0xff06] = %#02x\n", cpu.mmu.Read(0xff06))
+	// fmt.Printf("[0xff07] = %#02x\n", cpu.mmu.Read(0xff07))
+	// fmt.Printf("[0xff10] = %#02x\n", cpu.mmu.Read(0xff10))
+	// fmt.Printf("[0xff11] = %#02x\n", cpu.mmu.Read(0xff11))
+	// fmt.Printf("[0xff12] = %#02x\n", cpu.mmu.Read(0xff12))
+	// fmt.Printf("[0xff14] = %#02x\n", cpu.mmu.Read(0xff14))
+	// fmt.Printf("[0xff16] = %#02x\n", cpu.mmu.Read(0xff16))
+	// fmt.Printf("[0xff17] = %#02x\n", cpu.mmu.Read(0xff17))
+	// fmt.Printf("[0xff19] = %#02x\n", cpu.mmu.Read(0xff19))
+	// fmt.Printf("[0xff1a] = %#02x\n", cpu.mmu.Read(0xff1a))
+	// fmt.Printf("[0xff1b] = %#02x\n", cpu.mmu.Read(0xff1b))
+	// fmt.Printf("[0xff1c] = %#02x\n", cpu.mmu.Read(0xff1c))
+	// fmt.Printf("[0xff1e] = %#02x\n", cpu.mmu.Read(0xff1e))
+	// fmt.Printf("[0xff20] = %#02x\n", cpu.mmu.Read(0xff20))
+	// fmt.Printf("[0xff21] = %#02x\n", cpu.mmu.Read(0xff21))
+	// fmt.Printf("[0xff22] = %#02x\n", cpu.mmu.Read(0xff22))
+	// fmt.Printf("[0xff23] = %#02x\n", cpu.mmu.Read(0xff23))
+	// fmt.Printf("[0xff24] = %#02x\n", cpu.mmu.Read(0xff24))
+	// fmt.Printf("[0xff25] = %#02x\n", cpu.mmu.Read(0xff25))
+	// fmt.Printf("[0xff26] = %#02x\n", cpu.mmu.Read(0xff26))
+	// fmt.Printf("[0xff40] = %#02x\n", cpu.mmu.Read(0xff40))
+	// fmt.Printf("[0xff42] = %#02x\n", cpu.mmu.Read(0xff42))
+	// fmt.Printf("[0xff43] = %#02x\n", cpu.mmu.Read(0xff43))
+	// fmt.Printf("[0xff45] = %#02x\n", cpu.mmu.Read(0xff45))
+	// fmt.Printf("[0xff47] = %#02x\n", cpu.mmu.Read(0xff47))
+	// fmt.Printf("[0xff48] = %#02x\n", cpu.mmu.Read(0xff48))
+	// fmt.Printf("[0xff49] = %#02x\n", cpu.mmu.Read(0xff49))
+	// fmt.Printf("[0xff4a] = %#02x\n", cpu.mmu.Read(0xff4a))
+	// fmt.Printf("[0xff4b] = %#02x\n", cpu.mmu.Read(0xff4b))
+	// fmt.Printf("[0xffff] = %#02x\n", cpu.mmu.Read(0xffff))
 }
 
 func (cpu *CPU) PrintNextIns() {
@@ -71,6 +107,11 @@ func (cpu *CPU) PrintNextIns() {
 func (cpu *CPU) Fetch() uint8 {
 	res := cpu.mmu.Read(cpu.pc)
 	cpu.pc++
+
+	// after reaching 0x100, disable BIOS
+	if cpu.pc >= 0x100 {
+		cpu.mmu.IsBooting = false
+	}
 
 	return res
 }
--- main.go
+++ main.go
@@ -52,10 +52,11 @@ const (
 )
 
 var (
-	gpu        *g.GPU = g.New()
-	mmu        *m.MMU = m.New(gpu)
-	cpu        *c.CPU = c.New(mmu)
-	breakPoint uint16 = 0xfe
+	gpu *g.GPU = g.New()
+	mmu *m.MMU = m.New(gpu)
+	cpu *c.CPU = c.New(mmu)
+	// breakPoint uint16 = 0x100
+	// breakPoint uint16 = 0x282a // tetris end of tileset loading
 )
 
 func update(screen *ebiten.Image) error {
@@ -67,15 +68,18 @@ func update(screen *ebiten.Image) error {
 		// fmt.Printf("%#04x : ", cpu.GetPC())
 		// cpu.TestFlags()
 
-		if cpu.GetPC() == breakPoint {
-			isContinue := debugMode(cpu, &breakPoint)
-			if !isContinue {
-				break
-			}
-		} else {
-			ticks := cpu.Execute()
-			gpu.Update(ticks)
-		}
+		ticks := cpu.Execute()
+		gpu.Update(ticks)
+		// if cpu.GetPC() == breakPoint && !mmu.IsBooting {
+		// 	cpu.Dump()
+		// 	isContinue := debugMode(cpu, &breakPoint)
+		// 	if !isContinue {
+		// 		break
+		// 	}
+		// } else {
+		// 	ticks := cpu.Execute()
+		// 	gpu.Update(ticks)
+		// }
 
 	}
 
@@ -83,6 +87,7 @@ func update(screen *ebiten.Image) error {
 		return nil
 	}
 
+	// gpu.DisplayTileSets()
 	screen.ReplacePixels(gpu.Pixels)
 
 	msg := fmt.Sprintf("TPS = %0.2f\nFPS = %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS())
@@ -92,6 +97,26 @@ func update(screen *ebiten.Image) error {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Too few arguments. Please provide GameBoy ROM")
+		os.Exit(1)
+	}
+
+	fp, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	buf := make([]byte, 0x10000)
+	nb, err := fp.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Successfully read %d byte\n", nb)
+
+	mmu.Load(buf)
+
 	if err := ebiten.Run(update, screenWidth, screenHeight, 2, "Game Boy Emulator"); err != nil {
 		log.Fatal(err)
 	}
